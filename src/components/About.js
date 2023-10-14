5d2d4fcefe1dbc5e29a1bbb1cde59191b5243b40
Sravanthi9990 <kushma.saisravanthi2021@vitstudent.ac.in>
2023-10-14 13:22:25 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+// AboutMe.js
+import React from 'react';
+
+const About = () => {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <p>
+        Hi there! I'm, a dedicated and enthusiastic web Developer.
+      
+      </p>
+      
+    </section>
+  );
+};
+
+export default About;
+
---
+++ src/components/Contact.js
@@ -0,0 +1,31 @@
+// Contact.js
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <p>
+        I'm always open to new opportunities and collaborations. 
+      </p>
+      <ul>
+        <li>
+          <strong>Email:</strong> <a href="kkssravanthi2004@gmail.com ">Email</a>
+        </li>
+        <li>
+          <strong>LinkedIn:</strong> <a href=" https://www.linkedin.com/in/kumpatla-kushma-sai-sravanthi-ba9035251?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank" rel="noopener noreferrer">Linkedin</a>
+        </li>
+        <li>
+          <strong>GitHub:</strong> <a href="https://github.com/Sravanthi9990 " target="_blank" rel="noopener noreferrer">github</a>
+        </li>
+       
+      </ul>
+      <p>
+        I'm looking forward to connecting with you!
+      </p>
+    </section>
+  );
+};
+
+export default Contact;
+
